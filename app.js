ec2d2b0bdf7991a708e15aa345ef3a7c4b0be91f
Shaun <shaun@wove.co>
2022-03-04 15:35:14 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const keys = document.querySelectorAll("[data-audio]");
+
+keys.forEach((key) =>
+  key.addEventListener("click", () => {
+    const num = key.dataset.audio;
+    const sound = new Audio(`audio/key-${num}.mp3`);
+    sound.play();
+  })
+);
